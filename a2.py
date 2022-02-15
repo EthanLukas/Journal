a8657baedaffc4c88e38d807b61f20806ed406e5
Ethan Lukas <62733963+EthanLukas@users.noreply.github.com>
2022-02-15 01:29:58 -0800

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,218 @@
+# a2.py
+
+# Starter code for assignment 2 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Ethan Lukas
+# elukas@uci.edu
+# 19405892
+
+
+from genericpath import exists
+from pathlib import Path
+from tkinter import E
+import ui
+from Profile import Profile
+from Profile import Post
+
+
+profile = Profile()
+
+def run(admin, input1, command):
+    global profile
+    ''' Main function that manages inputs and conditions based on options and commands '''
+    #Try and except format used to handle errors gracefully, by printing that there is an error and then with the function is called again, requesting new input 
+    #The command as the first character in the input
+    if admin == True:
+    #Puts program into admin mode
+
+        input1 = input()
+        command = input1[:1]
+
+        while(command!='Q' and command!='q'):
+        #Loops until the user inputs to quit the program
+            try:
+                while((command.upper() != 'C' and command.upper() != 'O' and command.upper() != 'E' and command.upper() != 'P' and input1.upper() != 'Q') or len(input1)<=1):
+                #Requests new input while the command is invalid
+                    print('ERROR')
+                    input1 = input()
+                    command = input1[:1]
+
+                while(input1[1] != ''):
+                #Requests new input while the command is invalid
+                    print('ERROR')
+                    input1 = input()
+                    command = input1[:1]
+
+                if command.upper()=='C':                 
+                #Creates a new file in the specified directory 
+                    new_path = ui.admin_create(profile, input1)
+                    if new_path != 'opened':
+                        profile = Profile(dsuserver = new_path)
+                        profile.save_profile(new_path)
+
+                elif command.upper() == 'O':
+                #Loads a file
+                    try:
+                        path1 = input1[2:]
+                        ui.open_file(profile, path1)
+                    except:
+                        print('ERROR')
+                
+                elif command.upper() == 'E':
+                #Edits the dsu file contents
+                    ui.edit_file(profile, input1)
+
+                elif command.upper() == 'P':
+                #Prints data in the file loaded or created
+                    ui.print_file(profile, input1)
+
+                elif command.upper() == 'Q':
+                #Quits program
+                    quit()
+
+                input1 = input()
+                command = input1[:1]
+
+            except:
+            #Handles errors
+                if command=='Q' or command=='q':
+                    quit()
+                print('ERROR.')
+                input1 = input()
+                command = input1[:1]
+                run(admin, input1, command)
+        #Program quit when loop ends (q entered)        
+        quit()
+                
+              
+
+    elif admin == False: 
+        #Non-admin mode
+        try:
+            #Loops until user inputs Q to quit
+            while(input1!='Q' and input1!='q'):
+
+                while(input1.upper() != 'C' and input1.upper() != 'O' and input1.upper() != 'E' and input1.upper() != 'P' and input1.upper() != 'Q'):
+                #Requests new input while the command is invalid
+                    print('ERROR')
+                    print('What would you like to do next?')
+                    print('C. Create a new profile')
+                    print('O. Open/load an existing profile')
+                    print('E. Edit an existing profile')
+                    print('P. Print')
+                    print('Q. Quit')
+                    input1 = input()
+                    command = input1[:1]
+
+                if command.upper()=='C':                    
+                #Creates a new file 
+                    try:
+                        username1 = input("Enter username: ")
+                        password1 = input("Enter password: ")
+                        bio = input("Enter bio: ")
+                        path2 = input("Enter path: ")
+                        name1 = input('What would you like to name the profile: ')
+                        if name1.strip() != '':
+                            name1 = str(name1 + ".dsu")
+                            p = Path(path2) / name1
+                            new_path = str(path2 + "/" + name1)
+                            new_path1 = str(Path(path2) / name1)
+                            dir_path = Path(".") / path2
+
+                            #Checks if the path is valid
+                            if not dir_path.exists():
+                                print('ERROR. Invalid path.')
+                            else: 
+                                if p.exists():
+                                    ui.open_file(profile, new_path)
+                                    print("File already exists, loaded instead.")
+                                elif not p.exists():
+                                    if username1.strip() != '' and password1.strip() != '' and ui.whitespace(username1) == False and ui.whitespace(password1) == False:
+                                    #Checks if the username and password are empty or if they contain whitespace
+                                        p.touch()
+                                        profile = Profile(dsuserver = new_path1, username = username1, password = password1)
+                                        if bio.strip() != '':
+                                            profile.bio = bio
+                                        profile.save_profile(new_path1)
+                                    else:
+                                        print('ERROR')
+                        else:
+                            print('ERROR')
+                        
+                    except:
+                        print('ERROR')
+
+
+                elif command.upper() == 'O':
+                #Opens the file with given path
+                    try:
+                        path1 = input("Enter path to open: ")
+                        ui.open_file(profile, path1)
+                    except:
+                        print('ERROR')
+                
+
+                elif command.upper() == 'E':
+                #Edits file in user interface mode
+                    ui.interface_edit(profile)
+
+
+                elif command.upper() == 'P':
+                #Prints contents of file in user interface mode
+                    ui.interface_print(profile)
+
+                elif command.upper() == 'Q':
+                    quit()
+
+                else:
+                    print('ERROR')
+                
+
+                print('What would you like to do next?')
+                print('C. Create a new profile')
+                print('O. Open/load an existing profile')
+                print('E. Edit an existing profile')
+                print('P. Print')
+                print('Q. Quit')
+
+                #Takes input before looping again
+                input1 = input()
+                command = input1[:1]
+            
+            quit()
+
+        except:
+        #Error handlng
+            if command=='Q' or command=='q':
+                    quit()
+            print('ERROR.')
+            run(admin, input1, command)
+
+
+if __name__ == '__main__':
+    #Starts off the program with main menu
+    print('Welcome to the ICS 32 Journal! Input Q to exit the program')
+    print("Welcome! Do you want to create or load a DSU file (type 'c' to create or 'o' to open/load): ")
+
+    input1 = input()
+    command = input1[:1]
+    while(input1.upper() != 'C' and input1.upper() != 'O' and input1.upper() != 'Q' and input1.upper()!='ADMIN'):
+    #Requests new input while the command is invalid
+        print('ERROR. Try again')
+        input1 = input()
+        command = input1[:1]
+    if input1.upper() == 'Q':
+        quit()
+
+    #Checks whether to enter admin mode in run function
+    admin = False
+    
+    if input1.upper() == 'ADMIN':
+        admin = True
+        print('Welcome to Admin Mode')
+    else: 
+        admin = False
+
+    run(admin, input1, command)
---
+++ ui.py
@@ -0,0 +1,286 @@
+# ui.py
+
+# Starter code for assignment 2 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Ethan Lukas
+# elukas@uci.edi
+# 19405892
+
+from distutils import command
+from tkinter import W
+from Profile import Profile
+from Profile import Post
+from pathlib import Path
+import a2
+
+p = Path()
+def admin_create(profile: Profile, input1):
+    '''Creates a file in admin mode'''
+    try:
+        index2 = -1
+        #Finds path within input
+        for i in reversed(range(len(input1)-3)):
+            if (input1[i:i+4]==' -n '):
+                index2=i
+                break
+        path1 = input1[2:index2]
+        name_new_file =  input1[index2+4:]
+        name_new_file = name_new_file + '.dsu'
+        p = Path(path1) / name_new_file
+        dir_path = Path(".") / path1
+
+        if not dir_path.exists():
+        #Checks if the path is valid
+            print('ERROR. Invalid path.')
+            return 'opened'
+        else:
+            if p.exists():
+                path1 = str(Path(path1) / name_new_file)
+                open_file(profile, path1)
+                #returns opened to signify error
+                return 'opened'
+            elif not p.exists():
+            #If the file does not exist, it gets created
+                p.touch()
+        new_path = str(Path(path1) / name_new_file)
+        #Returns the path for a2 to use to save
+        return new_path
+    except:
+        print('ERROR')
+        #returns opened to signify error
+        return 'opened'
+
+
+
+def open_file(profile: Profile, path1):
+    ''' Opens/Loads a file '''
+
+    if path1[-4:] != '.dsu':
+    #Checks if the file is of dsu type
+        print('ERROR')
+        return
+
+    p = Path(".") / path1
+    if not p.exists():
+    #Checks if the file to be loaded exists
+        print('ERROR. Does not exist')
+        return
+    profile.load_profile(path1)
+    #Prints loaded file simple contents
+    print("Username is", profile.username)
+    print("Password is", profile.password)
+    if profile.bio != "":
+        print("Bio is", profile.bio)
+    print("File has been loaded successfully")
+
+
+def edit_file(profile: Profile, input1):
+    '''Edits contents of the file'''
+    try:
+        temp = input1[2:]
+
+        #Checks what to split by
+        #Depends on what type of quotations are used: double or single
+        ctr = 0
+        for i in input1:
+            if i=='\"':
+                ctr=1
+            elif i=='\'':
+                ctr=2
+        if ctr == 1:
+            c_list = temp.split("\"")
+            c_list = c_list[0:-1]
+        elif ctr == 2:
+            c_list = temp.split("\'")
+            c_list = c_list[0:-1]
+        else:
+            c_list = temp.split(" ")
+        #Loops through operations of editing
+        for i in range(0, len(c_list), 2):
+            strip_cmd = c_list[i].strip()
+            if strip_cmd == '-usr':
+            #Edit username
+                if not " " in c_list[i+1] and c_list[i+1].strip() != '':
+                    profile.username = c_list[i+1]
+                    profile.save_profile(profile.dsuserver)
+            elif strip_cmd == '-pwd':
+            #Edit password
+                if not " " in c_list[i+1] and c_list[i+1].strip() != '':
+                    profile.password = c_list[i+1]
+                    profile.save_profile(profile.dsuserver)
+            elif strip_cmd == '-bio':
+            #Edit bio
+                if c_list[i+1].strip() != '':
+                    profile.bio = c_list[i+1]
+                    profile.save_profile(profile.dsuserver)
+            elif strip_cmd == '-addpost': 
+            #Adds a post
+                if c_list[i+1].strip() != '':
+                    profile.add_post(Post(entry = c_list[i+1]))
+                    profile.save_profile(profile.dsuserver)
+            elif strip_cmd == '-delpost':
+            #Deletes a post based on index
+                try:
+                    profile.del_post(int(c_list[i+1]))
+                    profile.save_profile(profile.dsuserver)
+                except: 
+                    print('ERROR')
+                    return
+            else:
+            #If the operation doesn't exist
+                print('ERROR')
+                return
+    except:
+        print('ERROR')
+        return
+
+    
+def print_file(profile: Profile, input1):
+    '''Prints contents of the file'''
+    try:
+        temp1 = input1[2:]
+        command_list = temp1.split(" ")
+        #Loops through operations of printing
+        for i in range(0, len(command_list)):
+            cmd = command_list[i].strip()
+            if cmd == '-usr':
+            #Prints username
+                print("Username:", profile.username)
+            elif cmd == '-pwd':
+            #Prints password
+                print("Password:", profile.password)
+            elif cmd == '-bio':
+            #Prints bio
+                print("Bio:", profile.bio)
+            elif cmd == '-posts':
+            #Prints all posts
+                for post1 in range(len(profile._posts)):
+                    print(str(post1) + '.', profile._posts[post1].get_entry())
+            elif cmd == '-post':
+            #Prints specific post based on index
+                num1 = int(command_list[i+1])
+                print(str(i) + '.', profile._posts[num1].get_entry())
+            elif cmd == '-all':
+            #Prints all contents
+                print("Username:", profile.username)
+                print("Password:", profile.password)
+                print("Bio: ", profile.bio)
+                for post1 in range(len(profile._posts)):
+                    print(str(post1) + '.', profile._posts[post1].get_entry())
+    except:
+        print('ERROR')
+        return
+
+
+def interface_edit(profile: Profile):
+    '''Edits the file in user interface mode'''
+
+    print("What would you like to edit")
+    print("usr: Username")
+    print("pwd: Password")
+    print("bio: Bio")
+    print("addpost: Add a post")
+    print("delpost: Delete a post")
+    try:
+        cmd2 = input()
+        c_list = cmd2.split(" ")
+        #Loops through operations of editing in interface/non admin mode
+        #Checksto make sure entries are not empty strings
+        for i in range(0, len(c_list)):
+            cmd1 = c_list[i]
+            if cmd1 == 'usr':
+            #Edits the username
+                temp_user = input("Enter a new username: ")
+                if not " " in temp_user and temp_user.strip() != '':
+                    profile.username = temp_user
+                    profile.save_profile(profile.dsuserver)
+            elif cmd1 == 'pwd':
+            #Edits the password
+                temp_pass = input("Enter a new password: ")
+                if not " " in temp_pass and temp_pass.strip() != '':
+                    profile.password = temp_pass
+                    profile.save_profile(profile.dsuserver)
+            elif cmd1 == 'bio':
+            #Edits the bio
+                temp_bio = input("Enter a new bio: ")
+                if temp_bio.strip() != '':
+                    profile.bio = temp_bio
+                    profile.save_profile(profile.dsuserver)
+            elif cmd1 == 'addpost':
+            #Adds a post
+                entry1 = input("What entry to add: ")
+                if entry1.strip() != '':
+                    profile.add_post(Post(entry = entry1))
+                    profile.save_profile(profile.dsuserver)
+            elif cmd1 == 'delpost':
+            #Deletes a post
+                integer1 = input("What ID to delete: ")
+                try:
+                    profile.del_post(int(integer1))
+                    profile.save_profile(profile.dsuserver)
+                except:
+                    print('ERROR')
+                    return
+            else:
+            #For unknown operations entered
+                print('ERROR')
+                return
+    except:
+        print('ERROR')
+        return
+
+def interface_print(profile: Profile):
+    ''' Prints file contents in user interface mode '''
+    try:
+        print("What would you like to print")
+        print("usr: Username")
+        print("pwd: Password")
+        print("bio: Bio")
+        print("posts: Posts")
+        print("post: Specific post by ID")
+        print("all: All content stored in profile object")
+        cmd2 = input()
+        c_list = cmd2.split(" ")
+        #Loops through print operations
+        for i in range(0, len(c_list)):
+            cmd1 = c_list[i]
+            if cmd1 == 'usr':
+            #Prints username
+                print("Username:", profile.username)
+            elif cmd1 == 'pwd':
+            #Prints password
+                print("Password:", profile.password)
+            elif cmd1 == 'bio':
+            #Prints bio
+                print("Bio:", profile.bio)
+            elif cmd1 == 'posts':
+            #Prints posts
+                for post1 in range(len(profile._posts)):
+                    print(str(post1) + '.', profile._posts[post1].get_entry())
+            elif cmd1 == 'post':
+            #Prints specific post based on ID integer
+                num1 = int(input("ID of post (integer): "))
+                print(str(num1) + '.', profile._posts[num1].get_entry())
+            elif cmd1 == 'all':
+            #Prints all contents
+                print("Username:", profile.username)
+                print("Password:", profile.password)
+                print("Bio: ", profile.bio)
+                for post1 in range(len(profile._posts)):
+                    print(str(post1) + '.', profile._posts[post1].get_entry())
+            else:
+                print('ERROR')
+                return
+    except:
+        print('ERROR')
+        return
+
+def whitespace(str1):
+    ''' Checks string for whitespace '''
+
+    for i in str1:
+        if i == ' ':
+            return True
+    return False
