f3b72a84519be71caa65db618fcbcf4168311f90
Ethan Lukas <62733963+EthanLukas@users.noreply.github.com>
2022-02-15 01:40:57 -0800

    Update a2.py

--- a2.py
+++ a2.py
@@ -1,14 +1,3 @@
-# a2.py
-
-# Starter code for assignment 2 in ICS 32 Programming with Software Libraries in Python
-
-# Replace the following placeholders with your information.
-
-# Ethan Lukas
-# elukas@uci.edu
-# 19405892
-
-
 from genericpath import exists
 from pathlib import Path
 from tkinter import E
